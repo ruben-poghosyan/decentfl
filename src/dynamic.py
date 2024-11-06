9e01d7c0d51ae2db65e3356512f1d61f8274bc1f
ruben <poghosyan26@gmail.com>
2024-11-06 10:52:01 +0800

    add  k test

--- src/dynamic.py
+++ src/dynamic.py
@@ -1,25 +1,72 @@
+# Simulations for Ranking Mechanism
 import numpy as np
-def x2(x):
-    return x**3
-def minmax_scale(data, feature_range=(0, 1)):
-    """
-    Scales the input data using MinMax scaling.
+from matplotlib import pyplot as plt
+np.random.seed(0)
 
-    Parameters:
-    - data: array-like, the data to scale.
-    - feature_range: tuple (min, max), default is (0, 1) for scaling.
+# timesteps
+T = 1000
+#hyperparameters
 
-    Returns:
-    - Scaled data.
-    """
-    data = np.array(data)
-    min_val = np.min(data)
-    max_val = np.max(data)
+
+def simulate(k):
+    mu = 0.5
+    N = 500
+    N_mal = 0.5
+    state = [0 for _ in range(int(N - N*N_mal))]
+    state.extend([1 for _ in range(int(N*N_mal))])
+    np.random.shuffle(state)
+    alpha = np.random.normal(mu, 0.1, N)
+    r = []
+    for t in range(T):
+        # get current state estimates
+        if state.count(1) == 0:
+            break
+        r.append(state.count(0)/ state.count(1))
+        # simulate predictions
+
+        
+        # Remove some bad nodes
+        for _ in range(k):
+            if state.count(1) == 0:
+                break
+            ind = state.index(1)
+            state.pop(ind)
+            alpha = np.delete(alpha, len(alpha)-ind-1, 0)
     
-    # Scale the data
-    scaled_data = feature_range[0] + (data - min_val) * (feature_range[1] - feature_range[0]) / (max_val - min_val)
+    # Calculate Next State using random walk
+        temp = []
+        for a in alpha:
+            v = np.random.uniform(0, 1)
+            if v <= a:
+                temp.append(1)
+            else:
+                temp.append(0)
+        # update state
+        state = temp
+        # Update alphas and betas
+        temp = []
+        for (ind,a) in enumerate(alpha):
+            temp.append(a - np.random.normal(0.5, 1) + state.count(1)/(N*0.92))
+        alpha = temp
+    return r
+
+stop = 30
+x = np.arange(stop, step=1)
+
+styles = ['solid', (0, (5, 1)), (0, (5, 5)), (0, (1, 1)), (5, (10, 3))]
+for (i, k) in enumerate([1, 3, 5, 8, 10]):
+    r = simulate(k)
+    plt.plot(x, r[:stop], label=f'k={k}', linestyle=styles[i])
+
+plt.hlines(y=1,xmin=x[0], xmax=x[-1], colors='red', linestyles='dashed')
+plt.grid(visible=True, which='both')
+plt.ylabel('r', fontsize=12, rotation=0)
+plt.xlabel("Round")
+plt.legend()
+plt.savefig('k_diff.jpg', dpi=400, bbox_inches='tight')
     
-    return scaled_data
-x = np.linspace(-10,10,20)
-y = x2(x)
-print(x,y, minmax_scale(y))
+
+
+    
+
+
